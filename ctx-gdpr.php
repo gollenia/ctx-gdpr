296345b78d1e8ac7fb44eed9e8159ab6e148d1eb
Thomas Gollenia <thomas@gollenia.at>
2024-03-12 06:50:42 +0100

    version dump

--- ctx-gdpr.php
+++ ctx-gdpr.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     CTX GDPR 
  * Description:     Additional Blocks for GDPR
- * Version:         1.0
+ * Version:         1.0.1
  * Author:          Thomas Gollenia
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
